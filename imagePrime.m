4234256ff09dfcb562c38334b624f25b71ae95a4
chongxiaoxuan <71300510+chongxiaoxuan@users.noreply.github.com>
2020-12-25 13:17:48 +0800

    Update imagePrime.m

--- imagePrime.m
+++ imagePrime.m
@@ -4,38 +4,9 @@ function imagePrime()
 	% imagePrime()
 
     N=200; % 指定 N = 200
+    
     % 利用 clockwiseMatrix.m 的程式碼造出矩陣
-    if(N == 1)
-        A = 1;
-    elseif(rem(N,2) == 0)% 偶數的狀況(補矩陣的右，下)
-        A = zeros(N); % 建N*N的矩陣
-        B = clockwiseMatrix(N - 1); % N-1項的矩陣
-        for ii = 1 : N - 1 
-            % B[@,#]
-            % 行@: 將那一行的全部數字印出
-            % 列#: 將row1的最後一個數字每跑一次就+1
-            A(ii,:) = [B(ii,:),B(1,end)+ii]; 
-        end
-        for k = 1 : N
-            % 將矩陣最後一行印出
-            % +1是為了讓最後一行的第一個數字是屬於N^2
-            % k表示last row的next column
-            A(N,k) = N * N + 1 - k; 
-        end
-    else %奇數的狀況(補矩陣的左，上)
-        A = zeros(N);
-        B = clockwiseMatrix(N-1);
-        for ii = 2:N
-            % B[@,#]
-            % 行@: 將last row的最後一個數字做N-ii+1的動作，ii是每做一次的行數
-            % 列#: 將剩下的列全部數字印出
-            A(ii,:) = [B(end,1)+N-ii+1,B(ii-1,:)];
-        end
-        for k = 1:N
-            % row1
-            A(1,k) = N * N - N + k;
-        end
-    end
+    A = clockwiseMatrix(N);
     
     % 將質數的位子變成黑色，其餘的白色
     A = clockwiseMatrix(N);
