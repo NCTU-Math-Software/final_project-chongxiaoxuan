135ccf41be645b9f5cd5f0926860647b2d5e2f5b
chongxiaoxuan <71300510+chongxiaoxuan@users.noreply.github.com>
2020-12-25 14:08:28 +0800

    Update sumDiagonal.m

--- sumDiagonal.m
+++ sumDiagonal.m
@@ -5,9 +5,9 @@ function sum = sumDiagonal(N)
     % sumDiagonal(5)
     % sum = 101
    
-    sum = 0-rem(N,2); % 避免重複加到1，當N是奇數時
+    sum = 0-rem(N,2); % 避免重複加到1(當N是奇數時)
     for ii = 1:N
-        sum = (ii*ii-ii+1) + sum; %對角線
+        sum = (ii*ii-ii+1) + sum; % 正對角線
         % 反對角線
         if(rem(ii,2)==0) % 偶數情況
             sum = sum + ii * ii + 1;
